00ab2eca8bf75cbe69b5e8d0b7ec75b6b37aa807
Manches <Lukjanovilly@gmail.com>
2025-01-18 08:31:02 +0200

    finish-first-module

---
+++ js/module-1-variables.js
@@ -0,0 +1,125 @@
+//Variable and their types
+// const productName = "Droid";
+// console.log("productName:", productName)
+// const pricePerItem = "2000";
+// console.log("pricePerItem:", pricePerItem)
+
+// let productName = "Droid";
+// let pricePerItem = 2000;
+// //
+// productName = "Repair droid";
+// pricePerItem = 3500;
+// console.log(productName);
+// console.log(pricePerDroid);
+
+// let topSpeed = 160;
+// let distance = 617.54;
+// let login = "mango935";
+// let isOnline = true;
+// let isAdmin = false;
+// console.log(topSpeed);
+// console.log(distance);
+// console.log(login);
+// console.log(isOnline);
+// console.log(isAdmin);
+
+// const pricePerItem = 3500;
+// const orderedQuantity = 4;
+// //
+// // const totalPrice = pricePerItem * orderedQuantity;
+// console.log(totalPrice);
+
+// let a = 5;
+// let b = 10;
+// let c = 15;
+// let d = 20;
+// //
+// a += 2;
+// b -= 4;
+// c *= 3;
+// d /= 10;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+
+
+
+//String and their types
+// const username = "Poly";
+// let message = "Welcome " + username + "!";
+// console.log(message);
+
+// const productName = "Droid";
+// const pricePerItem = 3500;
+// //
+// const message = `You picked ${productName}, price per item is ${pricePerItem} credits`;
+// console.log(message);
+
+// let pricePerDroid = 800;
+// let orderedQuantity = 6;
+// let deliveryFee = 50;
+// //
+// let totalPrice = pricePerDroid * orderedQuantity + deliveryFee;
+// let message = `You ordered droids worth ${totalPrice} credits`;
+// console.log(totalPrice);
+// console.log(message);
+
+// const username = "Poly";
+// //
+// const message = `Username ${username} is ${username.length} characters long`
+// console.log(message);
+
+// const courseTopic = "JavaScript essentials";
+// //
+// const courseTopicLength = courseTopic.length;
+// const firstElement = courseTopic[0];
+// const lastElement = courseTopic[courseTopicLength - 1];
+// console.log(courseTopicLength);
+// console.log(firstElement);
+// console.log(lastElement);
+
+
+
+// Comparison operators
+// const age = 16;
+// const isAdult = age >= 18;
+// console.log(isAdult);
+
+// const correctPassword = "jqueryismyjam";
+// const userPassword = "mangodab3st";
+// const isValid = correctPassword === userPassword;
+// console.log(isValid);
+
+// const value = "24.5px";
+// const numerical = Number.parseFloat(value);
+// console.log(numerical);
+
+
+
+//Functions
+// function sayHi() {
+//   console.log("Hello, this is my first function!")
+// }
+//
+// sayHi()
+
+// function add(a, b, c) {
+//   console.log(`Addition result equals ${a + b + c}`);
+// }
+//
+// console.log("add(15, 27, 10):", add(15, 27, 10));
+// console.log("add(10, 20, 30):", add(10, 20, 30));
+// console.log("add(5, 10, 15):", add(5, 10, 15));
+
+// function makeMessage (name, price) {
+//     return (`You picked ${name}, price per item is ${price} credits`);
+// }
+// //
+// console.log("makeMessage("droids", 248):", makeMessage("droids", 248));
+
+// function calculateTotalPrice(orderedQuantity, pricePerItem) {
+//     return orderedQuantity*pricePerItem
+// }
+// //
+// console.log("calculateTotalPrice(8, 4500):", calculateTotalPrice(8, 4500));
